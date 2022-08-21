e03c7c8316acea6bf6be880307476447b099746c
Chathuranga97lj <chathuit97@gmail.com>
2022-08-21 07:18:34 +0530

    add files

---
+++ app.js
@@ -0,0 +1,12 @@
+const { required } = require("nodemon/lib/config");
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello API !');
+})
+
+app.listen(3000, () => {
+    console.log('server is running http://localhost:3000');
+})
