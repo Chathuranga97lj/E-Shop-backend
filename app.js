fc56b4b85927f2585ca62cf27b5c3c18f1932f30
Chathuranga97lj <chathuit97@gmail.com>
2022-08-24 07:22:15 +0530

    set category requests

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ app.use(cors());
 // http requests
 app.options('*', cors());
 
+// for identify post reqest json file format (Middleware)
+app.use(express.json());
+app.use(morgan('tiny'));
+
 // import routes
 const categoriesRoutes = require('./routers/categories');
-const productsRouter = require('./routers/products');
+const productsRouters = require('./routers/products');
 const usersRoutes = require('./routers/users');
 const ordersRoutes = require('./routers/orders');
 
@@ -20,13 +24,12 @@ const ordersRoutes = require('./routers/orders');
 // import api url
 const api = process.env.API_URL;
 
-// for identify post reqest json file format (Middleware)
-app.use(express.json());
-app.use(morgan('tiny'));
-
 
 // use api routers
-app.use(`${api}/products`, productsRouter);
+app.use(`${api}/products`, productsRouters);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
 
 // add befor starting the server 
