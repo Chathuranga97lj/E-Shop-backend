233797dae5d6b101d95cf8eafdbfad51ba0a7160
Chathuranga97lj <chathuit97@gmail.com>
2022-08-27 03:45:48 +0530

    set get request by ID

--- routers/categories.js
+++ routers/categories.js
@@ -2,15 +2,27 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
+// set get requrest
 router.get(`/`, async (req, res) => {
     const categoryList = await Category.find();
 
     if(!categoryList) {
         res.status(500).json({success: false})
     }
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 })
 
+// get category by ID
+router.get('/:id', async(req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not funded!'})
+    }
+    res.status(200).send(category);
+})
+
+// set post request
 router.post('/', async (req, res) => {
     let category = new Category({
         name: req.body.name,
