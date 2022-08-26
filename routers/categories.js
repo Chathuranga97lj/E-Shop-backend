ad454a2d7c783609b5e817ca18724310a5c37dda
Chathuranga97lj <chathuit97@gmail.com>
2022-08-27 04:08:32 +0530

    set delete category

--- routers/categories.js
+++ routers/categories.js
@@ -2,6 +2,7 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
+
 // set get requrest
 router.get(`/`, async (req, res) => {
     const categoryList = await Category.find();
@@ -12,6 +13,7 @@ router.get(`/`, async (req, res) => {
     res.status(200).send(categoryList);
 })
 
+
 // get category by ID
 router.get('/:id', async(req, res) => {
     const category = await Category.findById(req.params.id);
@@ -21,6 +23,7 @@ router.get('/:id', async(req, res) => {
     }
     res.status(200).send(category);
 })
+
 
 // set post request
 router.post('/', async (req, res) => {
@@ -35,6 +38,21 @@ router.post('/', async (req, res) => {
         return res.status(404).send('The category cannot be created !');
     
     res.send(category);
+})
+
+
+// set delete category
+// id: url look like (api/v1/sdgf24)
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then(category => {
+        if(category) {
+            return res.status(200).json({success: true, message: 'The category was deleted !'})
+        } else {
+            return res.status(200).json({success: false, message: 'The category is not funded !'})
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err})
+    })
 })
 
 module.exports = router;
