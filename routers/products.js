5f48f777da96cae75102b685b3adda61b618ce87
Chathuranga97lj <chathuit97@gmail.com>
2022-08-30 21:51:43 +0530

    find error with comment new get feature

--- routers/products.js
+++ routers/products.js
@@ -114,5 +114,15 @@ router.delete('/:id', (req, res) => {
     })
 })
 
+// get product count
+// router.get(`/get/count`, async (req, res) => {
+//     const productCount = await Product.countDocuments((count) => count);
+//     if(!productCount) {
+//         res.status(500).json({success: false})
+//     }
+//     res.send({
+//         productCount: productCount,
+//     });
+// })
 
 module.exports = router;
