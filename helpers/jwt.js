6ee9fefe650f751c40dcb38a5b0cf6c628e6f9cc
Chathuranga97lj <chathuit97@gmail.com>
2022-10-20 20:13:24 +0530

    set order schema

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,7 +14,7 @@ function authJwt() {
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`,
-            `${api}/users/get/count`,
+            // `${api}/users/get/count`,
         ]
     });
 }
