a64baf881ee3570bef42e8e098ddc2cba5bfa018
Chathuranga97lj <chathuit97@gmail.com>
2022-10-22 22:12:41 +0530

    get total price

--- routers/orders.js
+++ routers/orders.js
@@ -47,6 +47,15 @@ router.post('/', async (req, res) => {
     const orderItemsIdsResolved =  await orderItemsIds;
     //console.log(orderItemsIdsResolved); 
 
+    // calculate total price
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId)=>{
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice
+    }))
+    console.log(totalPrices);
+    const totalPrice = totalPrices.reduce((a, b) => a+b, 0);
+
     let order = new Order({
         orderItems: orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
@@ -56,7 +65,7 @@ router.post('/', async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user,
     })
     order = await order.save();
